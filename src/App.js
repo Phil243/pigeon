dd1ef9e03f7c3e7559bed095901fbda4bf7a008c
Philipp Strauß <phil13.strauss@gmail.com>
2022-04-22 13:34:08 +0200

    imported react-tabs, created tab component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+import MyTabs from './components/MyTabs';
+
 
 function App() {
   return (
@@ -7,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<MyTabs />
         </p>
         <a
           className="App-link"
